eb3be561de8bfb2b947036ce7177ee0a844fd7a0
aisha-partha <153170327+aisha-partha@users.noreply.github.com>
2024-08-23 12:14:40 +0530

    Adding research notebook

--- tests/test_features.py
+++ tests/test_features.py
@@ -29,3 +29,26 @@ def test_gender_variable_mapper(sample_input_data):
     assert subject.loc[1731, 'Gender'] == 1
 
 
+
+    
+
+def test_annual_premium_outlierhandler(sample_input_data):
+    # Given
+    encoder = OutlierHandler(variable = config.model_config.annual_premium_var)
+    q1, q3 = np.percentile(sample_input_data[0]['Annual_Premium'], q=[25, 75])
+    iqr = q3 - q1
+    print(q1)
+    
+    val = q3 + (1.5 * iqr)
+    print(type(sample_input_data))
+    res = sample_input_data[0][sample_input_data[0]['Annual_Premium'] > val]
+    print(res)
+    assert len(res) > 0
+    
+
+    # When
+    subject = encoder.fit(sample_input_data[0]).transform(sample_input_data[0])
+
+    # Then
+    reso = subject[subject['Annual_Premium'] > val]
+    assert len(reso) == 0
